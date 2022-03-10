3729e6bb1b9ed6f80446f53235cf0f54013f0822
githubzilla <githubzilla@gmail.com>
2022-03-10 22:19:46 +0800

    1st step

---
+++ setup_config.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+ln -s ./zshrc ~/.zshrc
+ln -s ./profile ~/.profile
+ln -s ./p10k.zsh ~/.p10k.zsh
+ln -s ./tmux.conf ~/.tmux.conf
+ln -s ./ssh ~/.ssh
+ln -s ./alacritty ~/.config/alacritty
+ln -s ./nvim ~/.config/nvim
