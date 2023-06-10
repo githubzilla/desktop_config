30c716e4fe5240b3bcdccdde486806609405fc13
githubzilla <githubzilla@gmail.com>
2023-06-10 09:55:37 +0800

    Add profile

--- setup_config.sh
+++ setup_config.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ln -s `pwd`/zshrc ~/.zshrc
-#ln -s `pwd`/profile ~/.profile
+ln -s `pwd`/profile ~/.profile
 ln -s `pwd`/p10k.zsh ~/.p10k.zsh
 ln -s `pwd`/tmux.conf ~/.tmux.conf
 #ln -s `pwd`/ssh ~/.ssh
